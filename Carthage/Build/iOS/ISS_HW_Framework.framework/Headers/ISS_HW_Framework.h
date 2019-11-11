c0f100c9e126e290931504d5ea2a5873ba2d0924
zudova-lada <55886908+zudova-lada@users.noreply.github.com>
2019-11-11 21:49:27 +0300

    Add files via upload

---
+++ Carthage/Build/iOS/ISS_HW_Framework.framework/Headers/ISS_HW_Framework.h
@@ -0,0 +1,19 @@
+//
+//  ISS_HW_Framework.h
+//  ISS-HW-Framework
+//
+//  Created by Лада on 11/11/2019.
+//  Copyright © 2019 Лада. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ISS_HW_Framework.
+FOUNDATION_EXPORT double ISS_HW_FrameworkVersionNumber;
+
+//! Project version string for ISS_HW_Framework.
+FOUNDATION_EXPORT const unsigned char ISS_HW_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ISS_HW_Framework/PublicHeader.h>
+
+
