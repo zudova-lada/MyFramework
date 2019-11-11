1cdb6b1d9ee2e9f49fae63d34417ed4ead279e3a
Lada Zudova <zudova.lada@mail.ru>
2019-11-11 21:47:44 +0300

    no message

---
+++ ISS-HW-Framework/Utils.swift
@@ -0,0 +1,19 @@
+//
+//  Utils.swift
+//  ISS-HW-Framework
+//
+//  Created by Лада on 11/11/2019.
+//  Copyright © 2019 Лада. All rights reserved.
+//
+
+import Foundation
+
+private class Utils {
+    
+    public init() {}
+    
+    public static func printMe() {
+        print("Printer")
+    }
+    
+}
