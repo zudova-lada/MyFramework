26ee198461495053f50ef7c9c983f84350a9cba3
Lada Zudova <zudova.lada@mail.ru>
2019-11-15 00:14:28 +0300

    no message

---
+++ TestFramework/Test.swift
@@ -0,0 +1,18 @@
+//
+//  Test.swift
+//  TestFramework
+//
+//  Created by Лада on 15/11/2019.
+//  Copyright © 2019 Лада. All rights reserved.
+//
+
+import Foundation
+
+open class MyTest{
+    public init(){}
+    
+    public func myPrint()
+    {
+        print("I belive you have a nice day")
+    }
+}
